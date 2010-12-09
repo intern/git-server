7f2cb43294f63d7eceb1279496de02508464805c
lan_chi <lan_chi@foxmail.com>
2010-12-09 15:54:32 +0800

    Some change server init file.

---
+++ git-server/authorized_keys_hook.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+"""
+Run this authorized keys hook to generate git authorized_keys file.
+"""
+
+import os, sys, re
+
+SSH_KEY_TEMPLATE = r'command="git-server %(ssh_id)d",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty %(ssh_key)s'
+
+
--- git-server/gitinit.py
+++ git-server/gitinit.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
@@ -16,7 +17,51 @@ COMMANDS_WRITE = [
     'git receive-pack',
     ]
 
+def initialization_db_sql():
+    return u"""
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+--
+-- 表的结构 `projects`
+--
+CREATE TABLE IF NOT EXISTS `{projects}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `description` text,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- 表的结构 `sshes`
+--
+
+CREATE TABLE IF NOT EXISTS `{sshes}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `ssh_key` text NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- 表的结构 `users`
+--
+CREATE TABLE IF NOT EXISTS `{users}` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+    """
+
 db_connect()
+
 def admin_config():
     return (
      'admin',
