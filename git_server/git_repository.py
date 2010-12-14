5e96584bc02fd2133187a406a45400c32efdc0d9
lan_chi <lan_chi@foxmail.com>
2010-12-14 12:01:28 +0800

    Add some "git_repository.py" methods.

--- git_server/git_repository.py
+++ git_server/git_repository.py
@@ -13,3 +13,21 @@ from db_base import *
 
 def commit_repository(path):
     pass
+
+
+def git_commit(full_path):
+    pass
+
+def git_initialize(full_path):
+    pass
+
+def git_copy(full_path, new_path):
+    pass
+
+def git_move(full_path, new_path):
+    pass
+
+def git_delete(full_path, new_path):
+    pass
+
+
