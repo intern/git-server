7f8a4429e998466495ee051ad00f7cb0e3a2e4b0
intern <lan_chi@foxmail.com>
2010-12-15 20:57:46 +0800

    Some change.

--- git_server/git_server.py
+++ git_server/git_server.py
@@ -18,11 +18,13 @@ ERROR_TIP = "Server is busy."
 
 CURRENT_USER = {}
 
+# have readonly access
 COMMANDS_READONLY = [
     'git-upload-pack',
     'git upload-pack',
     ]
 
+# have write access
 COMMANDS_WRITE = [
     'git-receive-pack',
     'git receive-pack',
@@ -33,10 +35,9 @@ def parser_ssh_id_from_command():
     c_handle = optparse.OptionParser()
     (options, args) = c_handle.parse_args()
     if not len(args):
-        logging.error("git-server params error, Is't ssh_id number, it is None")
+        logging.error("git-server params error, Please check the auth key file.")
         sys.exit(1)
     return int(args.pop(0))
-
 
 # return user info from {users} table.
 #     used parser_ssh_id_from_command()
@@ -63,12 +64,15 @@ def current_user():
 def handle_ssh_args():
     cmd = os.environ.get('SSH_ORIGINAL_COMMAND', None)
     if cmd is None:
-        logging.error("Must run this with ssh, becase SSH_ORIGINAL_COMMAND is None.")
+        logging.error("Must access this with ssh, because SSH_ORIGINAL_COMMAND is None!")
         sys.exit(1)
+
     logging.info("Runing command: %(cmd)r", dict(cmd = cmd))
+
     if '\n' in cmd:
-        logging.error("Error: Command may not contain newline: '%s'" % cmd)
+        logging.error("Command may not contain newline. :'%s'" % cmd)
         sys.exit(1)
+
     command, args = cmd.split(None, 1)
 
     # if command like "git upload-pack path_to_project.git"
@@ -76,22 +80,25 @@ def handle_ssh_args():
         try:
             sub_command, args= args.split(None, 1)
         except ValueError:
+            logging.error("Invalid ssh command: %s" % cmd)
             sys.exit(1)
         command = "%s %s" % (command, sub_command)
+
     if command not in COMMANDS_WRITE and command not in COMMANDS_READONLY:
-        logging.error("Error: Command '%s' is invalid!" % cmd)
+        logging.error("Command '%s' is invalid!" % cmd)
         sys.exit(1)
+
     match = ALLOW_RE.match(args)
     if match is None:
-        logging.error("Error: Command args '%s' is invalid! unsafe." % args)
+        logging.error("Command args '%s' is invalid! unsafe." % args)
         sys.exit(1)
+
     path = match.group('path')
 
-    logging.info("path=>%s; args=> %s" % (path,args))
-
     if path is None:
-        logging.error("path is None. exit")
+        logging.error("Git path is None, exit!")
         sys.exit(1)
+    # get current user info
     user = current_user()
 
     repor_path = standard_git_path(user, path)
@@ -105,35 +112,35 @@ def handle_ssh_args():
         command=command,
         path=full_path
         )
-    logging.info("Commiting....%s" % final_cmd)
+    logging.debug("Commiting....%s" % final_cmd)
+
     os.execvp('git', ['git', 'shell', '-c', final_cmd])
     
-    logging.info("ERROR.")
+    logging.error('"%s" Program execution failed.' % final_cmd)
 
 
 def standard_git_path(user, path):
     try:
         sub_path, repository = path.split('/')
     except ValueError:
-        logging.error("git...(%s)r" % path)
+        logging.error("git path error. Can't split with '/'" % path)
         sys.exit(1)
-    logging.error("user: %s: path :%s git: %s" % (user.get("login", "None"), sub_path, str(repository.endswith(".git"))))
+
     if sub_path == user.get('login', None) and repository.endswith(".git"):
         repository_name = repository[:-4]
         res = db_query("SELECT id FROM {projects} WHERE uid = %d AND name='%s'", user.get('id', None), repository_name)
         if not db_affected_rows(res):
-            logging.error("Project not find.")
+            logging.error("Project '%s' not found with user '%s'.", (repository, sub_path))
             sys.exit(1)
+
         return '/'.join([sub_path, repository])
-    return False    
+    return False 
 
 def bootstrap():
-#    os.system("echo %s >> /tmp/git_dev.log" % parser_command_ssh_id())
-
     # change to git user's home dir
     os.chdir(os.path.expanduser('~'))
     db_connect()
-    current_user()
+    #current_user()
     handle_ssh_args()
 
 
