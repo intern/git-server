af44eb3074316e02b931bbe0f94a81ddb43dcae5
intern <lan_chi@foxmail.com>
2010-12-26 13:51:39 +0800

    Update git web api.

--- git_server/git_web_api.py
+++ git_server/git_web_api.py
@@ -5,9 +5,11 @@
     - git_API_move
     - git_API_copy
     - git_API_delete
+    - git_API_exists
+    - git_API_run_key_hook
 """
 
-import sys
+import os, sys
 
 import git_status as EXIT
 
@@ -15,7 +17,7 @@ from optparse import OptionParser
 
 from git_repository import *
 
-GIT_WEB_APIs = ['git_API_init', 'git_API_rename', 'git_API_move', 'git_API_copy', 'git_API_delete', 'git_API_exists']
+GIT_WEB_APIs = ['git_API_init', 'git_API_rename', 'git_API_move', 'git_API_copy', 'git_API_delete', 'git_API_exists', 'git_API_run_key_hook']
 
 # API for git init a project
 #   params:
@@ -46,6 +48,10 @@ def git_API_delete(*args, **k_args):
 def git_API_exists(*args, **k_args):
     git_exists(*args, **k_args)
 
+# API for append ssh key to ~/.ssh/authorized_keys
+def git_API_run_key_hook(*args, **k_args):
+    return os.system("run-auth-keys-hook")
+
 # API main action handle to OP all sports
 def git_API_action():
     __action_boot()
@@ -64,4 +70,6 @@ def __action_boot():
         sys.exit(EXIT.API_NO_ACTION)
     API_callback = "git_API_%s" % options.action
     if API_callback in GIT_WEB_APIs:
-        globals()[API_callback](*args)        
+        globals()[API_callback](*args)
+    else:
+        sys.exit(EXIT.API_NO_ACTION)
